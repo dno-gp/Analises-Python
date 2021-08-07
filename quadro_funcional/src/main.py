3ffa93beddcf813f1374a16323f9c938beb0f575
dno-gp <dno.sales@gmail.com>
2021-08-07 15:21:14 -0300

    Update

--- quadro_funcional/src/main.py
+++ quadro_funcional/src/main.py
@@ -1,5 +1,5 @@
-'''Este programa retorna relatórios com a quantidade de funcionários 
-do município de Sapé/PB no ano de 2021.
+'''Este programa retorna relatórios com a quantidade de funcionários do
+município de Sapé/PB no ano de 2021.
 
 Data criação: 19/07/2021
 Versão 0.0.0
@@ -7,8 +7,7 @@ Versão 0.0.0
 Autor: Edson Sales <sales_eds@hotmail.com
 '''
 
-#  vscode://vscode.github-authentication/did-authenticate?windowid=1&
-# code=95d3002bb2010ab78402&state=3ef527cb-07a1-4032-8c36-07c2e795cf95 
+##############################################################################
 
 import sqlite3
 import pandas as pd
@@ -23,37 +22,47 @@ class Relatorio:
         comissionado = []
         contratado = []
 
+        ############################# COLETAR DADOS ##########################
         resultset = cur.execute("select sum(quantidade) from agentes \
-                                 where vinculo='efetivo' group by mes order by mes")
+                           where vinculo='efetivo' group by mes order by mes")
         for i in resultset:
             efetivo.append(i[0])
             
         resultset = cur.execute("select sum(quantidade) from agentes \
-                                 where vinculo='comissionado' group by mes order by mes")
+                       where vinculo='comissionado' group by mes order by mes")
         for i in resultset:
             comissionado.append(i[0])
 
         resultset = cur.execute("select sum(quantidade) from agentes \
-                                 where vinculo='contrato' group by mes order by mes")
+                           where vinculo='contrato' group by mes order by mes")
         for i in resultset:
             contratado.append(i[0])
 
-        # Exibindo os resultados:
+        ########################### EXIBIR RESULTADOS ########################
+        e1, e2, e3, e4, e5 = efetivo        # Os itens das listas são salvos
+        c1, c2, c3, c4, c5 = comissionado   # em variáveis como solução para
+        t1, t2, t3, t4, t5 = contratado     # eliminar os colchetes das listas 
+                                            # e a quantidade de código na 
+                                            # construção dessa exibição
+
         print(65*"-")
         print(f"{'Quantidade de servidores-Sapé/PB/2021':^65}")
+        print(f"{'GERAL':^65}")
         print(65*"-")
         print(f"{'Jan':>19}{'Fev':>9}{'Mar':>8}{'Abr':>8}{'Mai':>8}")
-        print(f"{'Efetivos:':>15}", str(efetivo[0]) + '\t', str(efetivo[1]) + '\t', str(efetivo[2]) + '\t', str(efetivo[3]) + '\t', 
-                                    str(efetivo[4]) + '\t')
+        print(f"{'Efetivos:':>15}", str(e1) + '\t', str(e2) + '\t',
+                                    str(e3) + '\t', str(e4) + '\t', 
+                                    str(e5) + '\t')
 
-        print(f"{'Comissionados:':>15}", str(comissionado[0]) + '\t', str(comissionado[1]) + '\t', str(comissionado[2]) + '\t', 
-                                         str(comissionado[3]) + '\t', str(comissionado[4]) + '\t',)
+        print(f"{'Comissionados:':>15}", str(c1) + '\t', str(c2) + '\t',
+                                         str(c3) + '\t', str(c4) + '\t',
+                                         str(c5) + '\t',)
 
-        print(f"{'Contratados:':>15}", str(contratado[0]) + '\t', str(contratado[1]) + '\t', str(contratado[2]) + '\t', str(contratado[3]) + '\t', 
-                                       str(contratado[4]) + '\t',)
+        print(f"{'Contratados:':>15}", str(t1) + '\t', str(t2) + '\t', 
+                                        str(t3) + '\t', str(t4) + '\t', 
+                                        str(t5) + '\t',)
         print(65*"-")
 
 if __name__=='__main__':
     a = Relatorio()
     a.geral()
-
