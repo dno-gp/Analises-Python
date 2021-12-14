6c31074702ce0c368fe93d87240c95cd3e29c9f1
dno-gp <dno.sales@gmail.com>
2021-12-14 13:51:17 -0300

    Novo

--- entrevista/main.py
+++ entrevista/main.py
@@ -1,2 +1,41 @@
-import pandas as pd
+# main.py - Estrutura de funcionamento do programa.
 
+class Entrevista:
+    respostas = []
+    continuar = "s"
+
+    def salva_resposta(*args):
+        respostas.append((*args))
+
+
+    def perguntas():
+        while continuar == "s":
+            
+            #sexo
+            sexo = input("Informe o sexo/gênero: ").lower()
+            if sexo != "m":
+                print("Resposta inválida.")
+                #idade
+            idade = input("Informe a idade: ")
+
+                #se interessa por política?
+            interesse = input("Se interessa por política?(0-não se interessa,\
+                                        1-pouco, 2-se interessa, 3-muito)")
+
+                #votou nas últimas eleições?
+            votou = input("Votou nas últimas eleições? ")
+
+"""  try:
+        salva_resposta((sexo, idade, interesse, votou))
+        print("Repostas registradas com sucesso!")
+    except:
+        print("As respostas não foram registradas")
+
+    continuar = input("Continuar? s-sim. ")
+    
+
+print(respostas)
+print(f"Total de entrevistados: {len(respostas)}")"""
+#Armazenar respostas em uma lista
+#Armazer lista em arquivo
+#Estatistica
