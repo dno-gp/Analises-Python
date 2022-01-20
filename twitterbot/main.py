f33c6a15da1eafdf47403e3664abd7b39caf27f6
dno-gp <dno.sales@gmail.com>
2022-01-20 00:47:18 -0300

    Envia tweet

--- twitterbot/main.py
+++ twitterbot/main.py
@@ -2,42 +2,41 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-import os
 import random
 import time
+import os
 
 class Bot:
     def __init__(self, login, pswd):
         self.login = login
         self.pswd = pswd
+        self.driver = webdriver.Firefox()
 
     def entrar(self):
-        driver = webdriver.Firefox()
-        driver.get("https://twitter.com/login?lang=pt")
-        time.sleep(random.randint(3,5))
+        self.driver.get("https://twitter.com/login?lang=pt")
+        time.sleep(5)
 
         #Login
-        elem_login = driver.find_element(
+        elem_login = self.driver.find_element(
                                         By.XPATH,
                                         "//input[@name='text'][@type='text']"
                                         )
         elem_login.click()
         time.sleep(2)
         elem_login.clear()
-        time.sleep(2)
         elem_login.click()
         time.sleep(random.randint(1,2))
         elem_login.send_keys(self.login)
         elem_login.send_keys(Keys.RETURN)
-        time.sleep(random.randint(3,5))
+        time.sleep(random.randint(3,4))
 
         #Password
-        elem_psw = driver.find_element(
+        elem_psw = self.driver.find_element(
                                       By.XPATH,
                                  "//input[@name='password'][@type='password']"
                                       )
         elem_psw.click()
-        time.sleep(random.randint(1,3))
+        time.sleep(random.randint(1,2))
         elem_psw.clear()
         elem_psw.click()
         time.sleep(random.randint(1,3))
@@ -49,7 +48,19 @@ class Bot:
 
     def twitter(self, tw):
         self.tw = tw
-        pass
+        self.driver.get("https://twitter.com/compose/tweet")
+        time.sleep(random.randint(2,3))
+        elem_box = self.driver.find_element(By.XPATH,
+                                           "//div[@role='textbox']")
+        time.sleep(random.randint(1,2))
+        for char in self.tw:
+            elem_box.send_keys(char)
+            time.sleep(random.randint(0,1))
+
+        elem_bt = self.driver.find_element(By.XPATH,
+                                          "//div[@role='button'][@data-testid='tweetButton']")
+        time.sleep(random.randint(1,2))
+        elem_bt.click()
 
     def comentar(self, comentario):
         self.comentario = comentario
@@ -65,5 +76,4 @@ class Bot:
 if __name__=='__main__':
     bot = Bot(os.environ.get('LOGIN_TW'), os.environ.get('PSW_TW'))
     bot.entrar()
-    bot.sair()
-
+    bot.twitter("Boa tarde!")
