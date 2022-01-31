640176ab88a6d18d3881a6e68ba3c835f4db52a1
dno-gp <dno.sales@gmail.com>
2022-01-31 11:46:19 -0300

    Loop

--- entrevista/main.py
+++ entrevista/main.py
@@ -1,27 +1,38 @@
 # main.py - Estrutura de funcionamento do programa Entrevistac.
-
+# autor:    Edson Sales<sales_eds@hotmail.com>
+#-----------------------------------------------------------------------------
 class Entrevista():
     def __init__(self):
         self.data = []
 
+    def perguntas(self):
+        continuar = "s"
+        while continuar == "s":
+            sexo = input("Sexo/gênero? ").lower()
+            idade = input("Qual sua idade? ")
+
+            flag = input("Salvar resposta? [s/n]").lower()
+            if flag == 's':
+                self.data.append((sexo, idade))
+                print("Respostas salvas com sucesso!")
+            elif flag == 'n':
+                print("As respostas não foram salvas.")
+            else:
+                print("Respostas não registradas.")
+
+            continuar = input('Ir para a próxima entrevista?[s/n]')
+            if continuar != "s":
+                print("Encerrando entrevista.")
+
     def respostas(self):
         print(self.data)
 
-    def perguntas(self):
-        sexo = input("Sexo/gênero? ").lower() # Esse bloco deve ser inserido
-        idade = input("Qual sua idade? ")     # em um loop.
+    def arquivar(self):
+        pass
 
-        flag = input("Salvar resposta? [s/n]").lower()
-        if flag == 's':
-            self.data.append((sexo, idade))
-            print("Respostas salvas com sucesso!")
-        elif flag == 'n':
-            print("As respostas não foram salvas.")
-        else:
-            print("Respostas não registradas.")
-
+    def estatistica(self):
+        pass
         
-
-
-exec = Entrevista()
-exec.perguntas()
+if __name__=='__main__':
+    exec = Entrevista()
+    exec.perguntas()
